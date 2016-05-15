90cfe05b730fca5f7256facd267835c01c1a6d4e
Bartosz Nowak <sztosz@gmail.com>
2016-05-15 16:58:22 +0200

    Update handle_response to handle new HTTPoison 0.8.0

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -12,11 +12,11 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response{%{status_code: 200, body: body}} do
+  def handle_response{:ok, %HTTPoison.Response{status_code: 200, body: body}} do
     { :ok, Poison.Parser.parse!(body) }
   end
 
-  def handle_response{%{status_code: ___, body: body}} do
-    { :error, Poison.Parse.parse!(body) }
+  def handle_response{:error, %HTTPoison.Error{reason: reason}} do
+    { :error, Poison.Parse.parse!(reason) }
   end
 end
